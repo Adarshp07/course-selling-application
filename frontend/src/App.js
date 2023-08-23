c02126cc064ea75f266e788e716bdfe0e0b83e98
Adarsh <pandey.adarshking@gmail.com>
2023-08-23 23:34:52 +0530

    Component initialised

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import Signup from "./Signup.js";
+import Signin from "./Signin.js";
+
+
+
+function App() {
+
+
+
+    return (
+        <>
+            <h1>Hello World</h1>
+
+            <div style={{
+                display: "flex"
+                , justifyContent: "space-between"
+                , color: "Blue"
+
+
+            }}>
+                <div>
+                    <h3>Coursera</h3>
+                </div>
+                <div>
+                    <h3>Signin</h3>
+                    <h3>Signup</h3>
+                </div>
+
+
+
+
+            </div >
+        </>)
+}
+
+export default App;
